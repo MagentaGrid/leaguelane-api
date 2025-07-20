7457015e4977bc366e0d13cd3fa7e94df7ff6712
Swaraj E S <swaraj.achu.50@gmail.com>
2025-07-21 00:58:01 +0530

    LGN-29 and LGN-30

---
+++ Leaguelane.Persistence/Entities/Entity.Audit.cs
@@ -0,0 +1,24 @@
+﻿using Leaguelane.Enums.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leaguelane.Persistence.Entities
+{
+    public class Audit: Entity
+    {
+        [Key]
+        public int AuditId { get; set; }
+
+        public Jobs JobId { get; set; }
+
+        public string JobName { get; set; }
+
+        public string Status { get; set; }
+
+        public string? Message { get; set; }
+    }
+}
