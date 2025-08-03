c5b47a3946bc01d4164f176f2828458a11b03303
vijai sebastian <vijaisebastian@gmail.com>
2025-08-03 01:12:17 +0100

    LGN-36 bet changes

---
+++ Leaguelane.Persistence/Migrations/20250728161227_bets28072025.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Leaguelane.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class bets28072025 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bets",
+                columns: table => new
+                {
+                    BetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LeagueId = table.Column<int>(type: "int", nullable: false),
+                    Season = table.Column<int>(type: "int", nullable: false),
+                    FixtureId = table.Column<int>(type: "int", nullable: false),
+                    FixtureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FixtureTimestamp = table.Column<long>(type: "bigint", nullable: false),
+                    Update = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedBy = table.Column<int>(type: "int", nullable: true),
+                    UpdatedBy = table.Column<int>(type: "int", nullable: true),
+                    Active = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bets", x => x.BetId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bets");
+        }
+    }
+}
