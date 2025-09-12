5a3b1a639fcdc30a613bc0df60ee2d94cffb0830
Swaraj E S <swaraj.achu.50@gmail.com>
2025-09-12 22:34:41 +0530

    Changes

--- Leaguelane.Scheduler/SchedulerConfiguration.cs
+++ Leaguelane.Scheduler/SchedulerConfiguration.cs
@@ -13,38 +13,37 @@ namespace Leaguelane.Scheduler
             {
                 q.UseMicrosoftDependencyInjectionJobFactory();
 
-                // Register all jobs
-                q.AddJob<CountryScheduler>(JobKey.Create(nameof(CountryScheduler)));
-                q.AddJob<LeagueScheduler>(JobKey.Create(nameof(LeagueScheduler)));
-                q.AddJob<SeasonsScheduler>(JobKey.Create(nameof(SeasonsScheduler)));
-                q.AddJob<FixtureScheduler>(JobKey.Create(nameof(FixtureScheduler)));
-                q.AddJob<RoundsScheduler>(JobKey.Create(nameof(RoundsScheduler)));
-                q.AddJob<TeamsScheduler>(JobKey.Create(nameof(TeamsScheduler)));
-                q.AddJob<TeamStatsScheduler>(JobKey.Create(nameof(TeamStatsScheduler)));
-                q.AddJob<BetScheduler>(JobKey.Create(nameof(BetScheduler)));
-                q.AddJob<BookmakerScheduler>(JobKey.Create(nameof(BookmakerScheduler)));
-                q.AddJob<OddsScheduler>(JobKey.Create(nameof(OddsScheduler)));
+                // Register jobs, marking those without triggers as durable
+                q.AddJob<CountryScheduler>(j => j.WithIdentity(nameof(CountryScheduler)));
+                q.AddJob<LeagueScheduler>(j => j.WithIdentity(nameof(LeagueScheduler)).StoreDurably());
+                q.AddJob<SeasonsScheduler>(j => j.WithIdentity(nameof(SeasonsScheduler)).StoreDurably());
+                q.AddJob<FixtureScheduler>(j => j.WithIdentity(nameof(FixtureScheduler)).StoreDurably());
+                q.AddJob<RoundsScheduler>(j => j.WithIdentity(nameof(RoundsScheduler)).StoreDurably());
+                q.AddJob<TeamsScheduler>(j => j.WithIdentity(nameof(TeamsScheduler)).StoreDurably());
+                q.AddJob<TeamStatsScheduler>(j => j.WithIdentity(nameof(TeamStatsScheduler)).StoreDurably());
+                q.AddJob<BetScheduler>(j => j.WithIdentity(nameof(BetScheduler)).StoreDurably());
+                q.AddJob<BookmakerScheduler>(j => j.WithIdentity(nameof(BookmakerScheduler)).StoreDurably());
+                q.AddJob<OddsScheduler>(j => j.WithIdentity(nameof(OddsScheduler)).StoreDurably());
 
-                // Only trigger the FIRST job (Country) daily
+                // Only the FIRST job gets a trigger
                 q.AddTrigger(t => t
-                    .ForJob(JobKey.Create(nameof(CountryScheduler)))
+                    .ForJob(nameof(CountryScheduler))
                     .WithIdentity("CountryTrigger")
                     .WithSimpleSchedule(s => s.WithIntervalInHours(24).RepeatForever())
                 );
 
-                // Create and configure job chain
+                // Define chaining order
                 var chain = new JobChainingJobListener("JobChain");
-                chain.AddJobChainLink(JobKey.Create(nameof(CountryScheduler)), JobKey.Create(nameof(LeagueScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(LeagueScheduler)), JobKey.Create(nameof(SeasonsScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(SeasonsScheduler)), JobKey.Create(nameof(FixtureScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(FixtureScheduler)), JobKey.Create(nameof(RoundsScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(RoundsScheduler)), JobKey.Create(nameof(TeamsScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(TeamsScheduler)), JobKey.Create(nameof(TeamStatsScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(TeamStatsScheduler)), JobKey.Create(nameof(BetScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(BetScheduler)), JobKey.Create(nameof(BookmakerScheduler)));
-                chain.AddJobChainLink(JobKey.Create(nameof(BookmakerScheduler)), JobKey.Create(nameof(OddsScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(CountryScheduler)), new JobKey(nameof(LeagueScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(LeagueScheduler)), new JobKey(nameof(SeasonsScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(SeasonsScheduler)), new JobKey(nameof(FixtureScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(FixtureScheduler)), new JobKey(nameof(RoundsScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(RoundsScheduler)), new JobKey(nameof(TeamsScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(TeamsScheduler)), new JobKey(nameof(TeamStatsScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(TeamStatsScheduler)), new JobKey(nameof(BetScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(BetScheduler)), new JobKey(nameof(BookmakerScheduler)));
+                chain.AddJobChainLink(new JobKey(nameof(BookmakerScheduler)), new JobKey(nameof(OddsScheduler)));
 
-                // Attach job listener
                 q.AddJobListener(chain);
             });
 
