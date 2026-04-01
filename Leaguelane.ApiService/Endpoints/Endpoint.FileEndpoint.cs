a5fe388f5813f61e8edad3c4e4c53cbe25661f10
Swaraj E S <swaraj.achu.50@gmail.com>
2026-04-01 19:30:01 +0530

    Image upload

---
+++ Leaguelane.ApiService/Endpoints/Endpoint.FileEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Leaguelane.ApiService.Feature;
+using Leaguelane.Constants.Enums;
+using Leaguelane.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaguelane.ApiService.Endpoints
+{
+    public static class FileEndpoint
+    {
+        public static RouteGroupBuilder AddFileRoutes(this RouteGroupBuilder group)
+        {
+            group.MapPost("", UploadFile).WithName("file-upload")
+                .DisableAntiforgery()
+                .RequireAuthorization(policy => policy.RequireRole(UserRole.Admin.ToString(), UserRole.Employee.ToString()));
+
+            return group;
+        }
+
+        public static async Task<IResult> UploadFile([FromServices] IFileFeatureService fileFeatureService, [FromForm] FileUploadRequest fileUploadRequest, CancellationToken cancellationToken)
+        {
+            var result = await fileFeatureService.UploadFile(fileUploadRequest.File, fileUploadRequest.Module, cancellationToken);
+            return TypedResults.Ok(result);
+        }
+    }
+}
---
+++ Leaguelane.Models/Dtos/Dto.FileUploadDto.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Leaguelane.Models.Dtos
+{
+    internal class FileUploadDto
+    {
+    }
+
+    public class FileUploadResult
+    {
+        public string Path { get; set; } = null!;
+        public string FileName { get; set; } = null!;
+        public long SizeBytes { get; set; }
+        public string ContentType { get; set; } = null!;
+        public DateTime UploadedAt { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class FileUploadRequest
+    {
+        public IFormFile File { get; set; } 
+        public string Module { get; set; }
+    }
+}
